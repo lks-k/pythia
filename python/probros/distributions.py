09b377fd383cc773c08b1e0ee5c5ca525c4c1a66
Markus Boeck <markus.boeck7800@hotmail.com>
2023-06-27 10:59:07 +0200

    add ppl backend to make models runnable + example notebook

---
+++ python/probros/distributions.py
@@ -0,0 +1,75 @@
+import random
+import math
+
+# to be able to run models we have to be able to draw a random value for a given distribution,
+# and compute the (log-) probability of this value.
+class Distribution:
+    def sample(self):
+        raise NotImplementedError
+    def logprob(self, value):
+        raise NotImplementedError
+
+
+# I have quickly implemented a few distributions,
+# but we can also use an existing package:
+# e.g. https://docs.pyro.ai/en/stable/distributions.html#pyro-distributions
+
+# We also have to consider that distribution packages have different conventions.
+# For example in Turing (Distributions.jl) Gamma(alpha,beta) corresponds to the parameterisation shape=alpha, rate=beta,
+# whereas in Pyro (torch.distributions) Gamma(k,theta) corresponds to the parameterisation shape=k, scale=theta
+# see https://en.wikipedia.org/wiki/Gamma_distribution
+
+class Bernoulli(Distribution):
+    # 1 with probability p, 0 with probability 1-p
+    def __init__(self, p):
+        self.p = p
+    def sample(self):
+        return random.random() < self.p
+    def logprob(self, value) -> float:
+        if value == 1:
+            return math.log(self.p)
+        else:
+            return math.log(1-self.p)
+    def __repr__(self) -> str:
+        return f"Bernoulli({self.p})"
+        
+class Dirac(Distribution):
+    # x with probability 1
+    def __init__(self, x):
+        self.x = x
+    def sample(self):
+        return self.x
+    def logprob(self, value) -> float:
+        if value == self.x:
+            return 0
+        else:
+            return -math.inf
+    def __repr__(self) -> str:
+        return f"Dirac({self.x})"
+        
+class Uniform(Distribution):
+    # pdf(x) = 1/(b-a) 1_[a,b](x)
+    def __init__(self, low, high):
+        self.low = low
+        self.high = high
+    def sample(self):
+        return self.low + (self.high - self.low) * random.random()
+    def logprob(self, value) -> float:
+        if self.low <= value and value <= self.high:
+            return -math.log(self.high-self.low)
+        else:
+            return -math.inf
+    def __repr__(self) -> str:
+        return f"Uniform({self.low}, {self.high})"
+    
+class Normal(Distribution):
+    # pdf(x) = 1/sqrt(2 pi sigma**2) * exp( -(x - mu)^2 / (2 sigma^2) )
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+    def sample(self):
+        return random.gauss(self.mu, self.sigma)
+    def logprob(self, value) -> float:
+        return -1/2 * ((value - self.mu) /self.sigma)**2 - math.log(self.sigma * math.sqrt(2*math.pi))
+    def __repr__(self) -> str:
+        return f"Normal({self.mu}, {self.sigma})"
---
+++ python/probros/sample.py
@@ -0,0 +1,88 @@
+from .distributions import *
+
+# global variables are ugly, but simple (pyro uses a similar method)
+_TRACE = {}
+
+# We define a decorator
+# @trace
+# def func():
+#     ...
+# which traces all sample, observe, and factor statements
+def trace(func):
+    def wrapper(*args, **kwargs):
+        global _TRACE
+        
+        # reset trace
+        _TRACE = {}
+
+        # run model function
+        retval = func(*args, **kwargs)
+
+        # sum up log probabilities, i.e. logprob is joint probability of model p(X,Y=y)
+        logprob = sum(entry['logprob'] for _, entry in _TRACE.items())
+
+        # store some more information
+        _TRACE['INPUT'] = (args, kwargs)
+        _TRACE['RETVAL'] = retval
+        _TRACE['LOGPROB'] = logprob
+
+        return retval, logprob, _TRACE
+    
+    return wrapper
+
+# sample statements have an address, because sometimes you may want to
+# have multiple statements corresponding to the same variable, but storing it
+# in different program identifiers. We require an explicit address (variable name)
+# e.g.
+# if test:
+#     x = sample("a", Bernoulli(0.9))
+#     ...
+# else:
+#     y = sample("a", Bernoulli(0.1))
+# Random value draws are logged in the trace at `address`.
+
+def sample(address: str, distribution: Distribution):
+    # draw random variable according to distribution
+    value = distribution.sample()
+
+    # compute log probability of sampled value
+    logprob = distribution.logprob(value)
+
+    # store result in global trace
+    _TRACE[address] = {
+        'kind': 'sample',
+        'value': value,
+        'logprob': logprob,
+        'distribution': distribution
+    }
+    # return sampled value
+    return value
+
+def observe(value, address: str = None, distribution: Distribution = Dirac(True)):
+    if address is None:
+        # we provide default (unique) addresses
+        address = f"observe_{len(_TRACE)}"
+
+    # compute log probability of observed value
+    logprob = distribution.logprob(value)
+
+    # store result in global trace
+    _TRACE[address] = {
+        'kind': 'observe',
+        'value': value,
+        'logprob': logprob,
+        'distribution': distribution
+    }
+
+    # return observed value
+    return value
+
+def factor(logprob, address: str = None):
+    if address is None:
+        address = f"factor:{len(_TRACE)}"
+
+    # store result in global trace
+    _TRACE[address] = {
+        'kind': 'factor',
+        'logprob': logprob
+    }
