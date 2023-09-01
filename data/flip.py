1ebd6e27d75a9cbd68cf2562da8107251fce0c5e
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2023-09-01 09:36:04 +0200

    Temporary commit, moving to another machine

--- data/flip.py
+++ data/flip.py
@@ -3,22 +3,35 @@ probprogs benchmark | flip.py
 Flip a binary coin and record the outcome. Possibly the simplest
 probabilistic program possible.
 """
-import numpy as np
-import time
-from scipy.stats import bernoulli
 
-np.random.seed(int(time.time()))
+import probros as pr
 
 """
 pre-expectation: [ ['x': 0] -> 0.5, 
                    ['x': 1] -> 0.5 ]
 """
-if bernoulli(0.5).rvs(): 
-    x = 0 
-else: 
-    x = 1
-
-print(f"{x}")
 """
 post-expectation: 1
 """
+@pr.trace
+def flip():
+  if pr.sample("coin", pr.Bernoulli(.5)): 
+      x = 0 
+  else: 
+      x = 1
+  return x
+    
+
+
+"""This is some thoughts on how to translate it"""
+
+def flip():
+  with pm.Model() as model:
+      pr_coin = pm.Bernouli ("coin", .5)
+      # difficult, requires data-flow analysis?
+      pr_x = pm.Deterministic ("x", 0 if pr_coin else 1)
+  return model
+
+m = flip()
+trace = m.sample()
+# here put some basic output
